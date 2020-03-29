87dbd5f49c9554eb21c0ea811e1cef506af03bc4
Shivam Agrawal <shivam.agrawal@lntinfotech.com>
2020-03-29 23:32:27 +0530

    initial commit

---
+++ precache-manifest.48914b92caa4ab0b7ff813569743c720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1aaa3047c471e8d2d2bbcb9d3d04009",
+    "url": "/corvis/index.html"
+  },
+  {
+    "revision": "4e8f31fadba65bdb5b55",
+    "url": "/corvis/static/css/main.7ff3e9c6.chunk.css"
+  },
+  {
+    "revision": "cafb35d19899386c11cc",
+    "url": "/corvis/static/js/2.f4f99203.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corvis/static/js/2.f4f99203.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8f31fadba65bdb5b55",
+    "url": "/corvis/static/js/main.2ee3fb47.chunk.js"
+  },
+  {
+    "revision": "c64605eb4c40ec8d76a5",
+    "url": "/corvis/static/js/runtime-main.8e1d5745.js"
+  }
+]);
